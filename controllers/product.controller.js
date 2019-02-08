466b323b67c2468015a1cbf3a6def01f8fe7e14f
Danielle Soares da Silva <danizinha.bruxinha@gmail.com>
2019-02-08 17:51:58 -0200

    Initial commit

---
+++ controllers/product.controller.js
@@ -0,0 +1,39 @@
+const Product = require('../models/product.model');
+
+exports.product_create = function (req, res, next) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+exports.product_details = function (req, res, next) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_update = function (req, res, next) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
